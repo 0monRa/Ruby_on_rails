b5ecd33c6ce98ee8fd1e4c2726916b111d4bc18a
Gr1shka12 <166213671+Gr1shka12@users.noreply.github.com>
2024-06-04 16:03:13 +0300

    models tests

--- test/models/breed_test.rb
+++ test/models/breed_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class BreedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @breed = Breed.new(name: "Siamese")
+  end
+
+  test "should be valid" do
+    assert @breed.valid?
+  end
+
+  test "name should be present" do
+    @breed.name = "  "
+    assert_not @breed.valid?
+  end
+
+  test "path should be set before create if not present" do
+    @breed.save
+    assert_equal @breed.name, @breed.path
+  end
+
+  test "path should not be overridden if already set" do
+    @breed.path = "custom_path"
+    @breed.save
+    assert_equal "custom_path", @breed.path
+  end
 end
--- test/models/cat_test.rb
+++ test/models/cat_test.rb
@@ -1,7 +1,33 @@
-require "test_helper"
+require 'test_helper'
 
 class CatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @breed = Breed.create(name: "Siamese")
+    @cat = Cat.new(name: "Whiskers", breed: @breed)
+  end
+
+  test "should be valid" do
+    assert @cat.valid?
+  end
+
+  test "name should be present" do
+    @cat.name = "  "
+    assert_not @cat.valid?
+  end
+
+  test "breed should be present" do
+    @cat.breed = nil
+    assert_not @cat.valid?
+  end
+
+  test "path should be set before create if not present" do
+    @cat.save
+    assert_equal @cat.name, @cat.path
+  end
+
+  test "path should not be overridden if already set" do
+    @cat.path = "custom_path"
+    @cat.save
+    assert_equal "custom_path", @cat.path
+  end
 end
